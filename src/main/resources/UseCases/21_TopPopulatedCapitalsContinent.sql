d37c1d583e4023ad784ecf8efbea6d9413e225e0
Faith <120490661+FaithTV@users.noreply.github.com>
2025-11-06 22:21:56 +0000

    Fully refactoring the code to run the SQL instead of the Employee code! Imported the database, updated the docker compose to run off the world database, and now working on recoding the app to run the files inside of the SQL (COMMENTED CORRECTLY NOW)

--- src/main/resources/UseCases/21_TopPopulatedCapitalsContinent.sql
+++ src/main/resources/UseCases/21_TopPopulatedCapitalsContinent.sql
@@ -5,11 +5,11 @@ USE world;
 -- Selecting the capitals, countries and population
 SELECT
     city.Name AS 'Capital Name',
-        country.Name AS 'Country',
-        city.Population AS 'Population'
+    country.Name AS 'Country',
+    city.Population AS 'Population'
 FROM country
          JOIN city ON country.Capital = city.ID
 -- Filter by continent name
 WHERE country.Continent = 'Europe'
 ORDER BY city.Population DESC
-    LIMIT 10; -- that`s where the number provided by the user be
+LIMIT 10; -- that`s where the number provided by the user be
