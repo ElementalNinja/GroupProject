bb72bd95be71e95aae3187c789e59677fb791e65
Tt-minus <40729616@live.napier.ac.uk>
2025-11-07 11:58:48 +0000

    All cities in a continent sorted by largest to smallest
    Fix SQL query to select and order cities by population.

---
+++ src/main/resources/UseCases/8_CitiesInContinent.sql
@@ -0,0 +1,8 @@
+/*All the cities in a continent organised by largest population to smallest. */
+SELECT
+  country.Continent AS 'Continent'
+  city.Population AS 'Population'
+FROM country
+  JOIN city ON country.Capital = city.ID
+GROUP BY country.Continent
+ORDER BY 'Population' DESC;
