4febf03102fbbe653e6ce4a9493cf45dd483cc3e
Tt-minus <40729616@live.napier.ac.uk>
2025-11-07 12:05:32 +0000

    Fix SQL query to sum city populations by region

--- src/main/resources/UseCases/9_CitiesInRegion.sql
+++ src/main/resources/UseCases/9_CitiesInRegion.sql
@@ -1,8 +1,8 @@
 /*All the cities in a region organised by largest population to smallest. */
 SELECT
-  country.Region AS 'Region'
-  city.Population AS 'Population'
+  country.Region AS 'Region',
+  SUM(city.Population) AS 'Population'
 FROM country
   JOIN city ON country.Capital = city.ID
-GROUP BY country.Region, city.Population
+GROUP BY country.Region
 ORDER BY 'Population' DESC;
