2daa85ce32b3daf56132c8821f2ccf95fcbdb758
Tt-minus <40729616@live.napier.ac.uk>
2025-11-07 12:08:40 +0000

    Fix SQL query to include city name in selection

--- src/main/resources/UseCases/7_CitiesInWorld.sql
+++ src/main/resources/UseCases/7_CitiesInWorld.sql
@@ -1,5 +1,6 @@
 /*All the cities in the world organised by largest population to smallest. */
 SELECT
-  city.population AS 'Population'
+  city.Name As 'City',
+  city.Population AS 'Population'
 FROM city
 ORDER BY 'Population' DESC;
