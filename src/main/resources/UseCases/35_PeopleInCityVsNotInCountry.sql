d5b693bef4aaf94921d3f30e9e9751e454ca0241
Tt-minus <40729616@live.napier.ac.uk>
2025-11-09 15:12:30 +0000

    Added percent columns for use case 35

--- src/main/resources/UseCases/35_PeopleInCityVsNotInCountry.sql
+++ src/main/resources/UseCases/35_PeopleInCityVsNotInCountry.sql
@@ -1,13 +1,15 @@
 /*Population Report  For the population reports, the following information is requested:
-The name of the continent/region/country.
-The total population of the continent/region/country.
-The total population of the continent/region/country living in cities (including a %).
-The total population of the continent/region/country not living in cities (including a %).*/
+The name of the country.
+The total population of the country.
+The total population of the country living in cities (including a %).
+The total population of the country not living in cities (including a %).*/
 SELECT
   country.Name AS 'Name',
   SUM(country.Population) AS 'Total Population',
-  SUM(city.Population) AS 'City Population', 
-  (SUM(country.population)-SUM(city.Population)) AS 'Not in city'
+  SUM(city.Population) AS 'City Population',
+  CONCAT(ROUND(SUM(city.Population)/Sum(country.Population) * 100),'%') AS 'Percent In City',
+  (SUM(country.population)-SUM(city.Population)) AS 'Not in city',
+  CONCAT(ROUND((SUM(country.population)-SUM(city.Population))/Sum(country.Population) * 100),'%') AS 'Percent Not In City'
 FROM country
   JOIN city ON country.Capital = city.ID
 GROUP BY country.Name;
