95dacbf3783b8272853f3ed10515bd8c00b4d3c6
Jack <40772359@live.napier.ac.uk>
2025-11-11 17:25:16 +0000

    Fixed Use Case 32

--- src/main/resources/UseCases/32_CountryReport.sql
+++ src/main/resources/UseCases/32_CountryReport.sql
@@ -1,6 +1,7 @@
 SELECT country.Code, country.Name,
        country.Continent, country.Region,
-       country.Population, country.Capital
+       country.Population, city.Name
 FROM country
 LEFT JOIN city ON country.Capital = city.ID
+
 ORDER BY country.Code
