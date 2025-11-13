3ca572ff897161456fbdf1ef0206e17fc706a93c
ElementalNinja <cameroncstanton@gmail.com>
2025-11-13 17:58:59 +0000

    Created the unit testing app

---
+++ src/test/java/com/napier/sem/UnitTesting.java
@@ -0,0 +1,14 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class UnitTesting
+{
+    @Test
+    void unitTest1()
+    {
+        assertEquals(5, 5);
+    }
+}
