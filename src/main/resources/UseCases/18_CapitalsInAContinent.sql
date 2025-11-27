b2e592f6a07da3cae87414e17e70290ff2357eed
Jack <40772359@live.napier.ac.uk>
2025-11-27 22:54:07 +0000

    Updated Query 18

--- src/main/resources/UseCases/18_CapitalsInAContinent.sql
+++ src/main/resources/UseCases/18_CapitalsInAContinent.sql
@@ -1,5 +1,6 @@
-﻿SELECT city.Name, Continent, city.Population
+﻿SELECT city.Name, country.Continent, city.Population
 FROM country
 JOIN city ON country.Capital = city.ID
 WHERE country.Continent = 'Europe'
+
 ORDER BY city.Population DESC;
